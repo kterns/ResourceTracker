f8ed3bdb0c2bbf7013370ad67349802b59b1394b
Kyle <ternsk2@winthrop.edu>
2016-03-13 16:59:03 -0400

    Removed GA and made json call relative

--- client/components/responseTracker/trackerservice.js
+++ client/components/responseTracker/trackerservice.js
@@ -2,8 +2,6 @@
 
 app.service('responseTrackerService', function($http) {
 	this.getData = function() {
-		return $http.get("http://localhost:8079/assets/downloads/api.json");
-		// return $http.get("http://192.168.0.23/response-tracker/assets/downloads/api.json");
-		// return $http.get("http://kterns.com/response-tracker/assets/downloads/api.json");
+		return $http.get("assets/downloads/api.json");
 	}
 });
